ac8031c78bb1dfdb897af09e5023bb9cd422e234
jonepl <plj.coding@gmail.com>
2024-11-10 18:14:24 -0500

    Code Cleanup

--- init-swarm.local.sh
+++ init-swarm.local.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 APP_WORKING_DIR="/srv/app"
 VHOST_DIR="$APP_WORKING_DIR/api-gateway/vhost.d"
-ENV_CONFIG="$APP_WORKING_DIR/app-scripts/.env"
+APP_SCRIPT_DIR="$APP_WORKING_DIR/app-scripts"
+ENV_CONFIG="$APP_SCRIPT_DIR/.env"
+PROJECT_DIR="$(dirname $(pwd))"
 CURRENT_DIR=$(pwd)
+
 
 log_message() {
     local level="$1"
@@ -22,7 +25,7 @@ initialize_env_vars() {
         REQUIRED_VARS=(DOMAIN)
         for VAR in "${REQUIRED_VARS[@]}"; do
             if [ -z "${!VAR}" ]; then
-                log_message "ERROR" "$VAR is not set in $ENV_CONFIG"
+                echo "Error: $VAR is not set in $ENV_CONFIG"
                 exit 1
             fi
         done
@@ -49,7 +52,8 @@ create_network() {
     # Create reverse proxy network
     if ! docker network ls | grep -q "portfolio-network"; then
         log_message "INFO" "Creating Docker overlay network 'portfolio-network'..."
-        docker network create --driver overlay --attachable portfolio-network
+        docker network create --driver overlay portfolio-network
+        # docker network create --driver overlay --attachable portfolio-network
     else
         log_message "INFO" "Docker overlay network 'portfolio-network' already exists."
     fi
@@ -57,7 +61,8 @@ create_network() {
     # Create Portainer Agent network
     if ! docker network ls | grep -q "agent-network"; then
         log_message "INFO" "Creating Docker overlay network 'agent-network'..."
-        docker network create --driver overlay --attachable agent-network
+        docker network create --driver overlay agent-network
+        # docker network create --driver overlay --attachable agent-network
     else
         log_message "INFO" "Docker overlay network 'agent-network' already exists."
     fi
@@ -75,40 +80,70 @@ create_volume() {
 
 # Start services
 start_services() {
-    log_message "INFO" "Deploying Services"
+    log_message "INFO" "Deploying Services ..."
     mkdir -p $VHOST_DIR
 
-    # Load environment variables and deploy CSV Merger API
+    # Deploy CSV merger API and web portfolio
     docker stack deploy -c $APP_WORKING_DIR/csv-merger-api/docker-compose.yml -c $APP_WORKING_DIR/web-portfolio/docker-compose.yml hosted-apps
 
-    # Deploy Portainer
-    docker stack deploy -c $APP_WORKING_DIR/site-reliability-tools/maintenance/docker-compose.yml sre-tools
+    # Deploy Certbot and Portainer
+    docker stack deploy -c $APP_WORKING_DIR/site-reliability-tools/security/docker-compose.local.yml -c $APP_WORKING_DIR/site-reliability-tools/maintenance/docker-compose.yml sre-tools
 
-    # Load environment variables and deploy API Gateway
+    # Deploy API gateway
     docker stack deploy -c $APP_WORKING_DIR/api-gateway/docker-compose.yml hosted-apps
-
-    # Wait for API Gateway to be started before deploying Certbot
-    # NOTE: This is done to prevent failures when new domains are added
-    wait_for_api_gateway
-    docker stack deploy -c $APP_WORKING_DIR/site-reliability-tools/security/docker-compose.yml sre-tools
 }
 
-wait_for_api_gateway() {
-    log_message "INFO" "Waiting for API gateway on $DOMAIN to be up..."
+wait_for_certbot() {
+    local retries=15
+    local count=0
+    local sleep_time=5
 
-    while ! curl -s --head  --request GET "$DOMAIN" | grep "301 Moved Permanently" > /dev/null; do
-        log_message "INFO" "API gateway is not up yet. Waiting..."
-        sleep 10
+    log_message "INFO" "Waiting for Certbot container to be ready..."
+
+    while [ $count -lt $retries ]; do
+        CERTBOT_CONTAINER_ID=$(docker ps -qf "name=certbot")
+
+        if [ -n "$CERTBOT_CONTAINER_ID" ]; then
+            log_message "INFO" "Certbot container is running with ID: $CERTBOT_CONTAINER_ID"
+            return 0
+        else
+            log_message "INFO" "Certbot container is not ready yet. Waiting for $sleep_time seconds..."
+            sleep $sleep_time
+            count=$((count + 1))
+        fi
     done
 
-    log_message "INFO" "API gateway is up. Proceeding with Certbot script."
+    log_message "INFO" "Certbot container did not become ready after $((retries * sleep_time)) seconds."
+    return 1
+}
+
+# Copy certificates from Certbot to DinD container
+copy_certs() {
+    log_message "INFO" "Copying certificates from Certbot container to mock server (DinD) container..."
+
+    # Get the Certbot container ID
+    CERTBOT_CONTAINER_ID=$(docker ps -qf "name=certbot")
+    # Define paths
+    SELF_SIGNED_CERT_PATH="/etc/letsencrypt/live/$DOMAIN"
+    LOCAL_CERT="localhost.crt"
+    LOCAL_CERT_KEY="localhost.key"
+    DEST_PATH="/tmp/host-certs"
+
+    # Ensure destination directory exists
+    mkdir -p $DEST_PATH
+
+    # Copy certificates from service container to mock server
+    docker cp $CERTBOT_CONTAINER_ID:$SELF_SIGNED_CERT_PATH/$LOCAL_CERT $DEST_PATH/$LOCAL_CERT
+    docker cp $CERTBOT_CONTAINER_ID:$SELF_SIGNED_CERT_PATH/$LOCAL_CERT_KEY $DEST_PATH/$LOCAL_CERT_KEY
+
+    log_message "INFO" "Certificates copied successfully."
 }
 
 create_secret() {
     docker secret create app_config $ENV_CONFIG
 }
 
-log_message "INFO" "Starting apps..."
+echo "Starting apps..."
 cd $APP_WORKING_DIR
 
 initialize_env_vars
@@ -122,6 +157,13 @@ create_volume
 create_secret
 
 start_services
+
+# Wait for Certbot container to be ready before copying certificates
+if wait_for_certbot; then
+    copy_certs
+else
+    log_message "ERROR" "Failed to copy certificates because Certbot container is not ready."
+fi
 
 cd $CURRENT_DIR
 log_message "INFO" "Finished running script!"
